a39427f50d10019e216db6aeecf90d32667d9ba8
the.faizan.raza <the.faizan.raza@gmail.com>
2020-02-15 03:00:38 +0500

    trying fixing bug 7

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ const session = require('express-session')
 
 
     const db = new Client({
-        host     : 'localhost',
-        user     : 'postgres',
-        password : '123',
-        database : 'postgresql-parallel-65900'
+        host     : 'ec2-34-200-116-132.compute-1.amazonaws.com',
+        user     : 'zdhjbiamcezysx',
+        password : 'cf506a4a4544af0d2760875fbb29a54566d794a054c798e52b8a6a5f5c6ceea0',
+        database : 'd5ael86tl17shu'
       });
 
 
